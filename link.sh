a5c815ae590fb86329845cd9ef88c7eac089926c
jumboly <jumboly@gmail.com>
2020-11-03 11:03:55 +0900

    ツールのリンクを作成

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+ln -s ./tools/Assembler.sh as & chmod +x as
+ln -s ./tools/CPUEmulator.sh ce & chmod +x ce
+ln -s ./tools/HardwareSimulator.sh hs & chmod +x hs
+ln -s ./tools/JackCompiler.sh jc & chmod +x jc
+ln -s ./tools/TextComparer.sh tc & chmod +x tc
+ln -s ./tools/VMEmulator.sh ve & chmod +x ve
+
